f5f38c743b462ca77ea9961f67db9688025b31a1
the-cc-dev <the.cc.dev@gmail.com>
2017-09-11 19:14:41 -0500

    before transfer
    created new project, saving this before transferring new files into
    this repository

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,18 @@
-        </div>
-      </main>
+</div></main>
 
-      <footer id="theFooter" class="the-footer">
-        <div class="footer-bottom"></div>
-      </footer>
+
+
+	<footer id="theFooter" class="the-footer">
+		<hr class="footer-item">
+		<div id="footerInfo" class="footer-item footer-info">
+			<span class="footer-info-item copyright-brand">&copy; McClanahan Design</span>
+			<span class="footer-info-item phone-number"><?php the_field('phone_number','options'); ?></span>
+		</div>
+		
+		<div class="footer-item footer-bottom"></div>
+		
+	</footer>
       
-    </div>
-  </body>
-</html>
+
+
+</div></body></html>
--- templates/navbar-header.php
+++ templates/navbar-header.php
@@ -1,6 +1,6 @@
 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#theNavbar">
     <i class="fa fa-bars"></i>
 </button>
-<a class="navbar-brand" href="http://localhost:3000/metalwork/">
+<a class="navbar-brand" href="<?php home_url(); ?>">
     <?php bloginfo('name'); ?>
 </a>
